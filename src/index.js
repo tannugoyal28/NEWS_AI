8ac4b56d550a34ad08334376c984bbc40384438f
tannugoyal28 <gtannu563@gmail.com>
2023-06-16 16:08:34 +0530

    inital commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import { createRoot } from 'react-dom/client'
 
 import './index.css';
 
-import App from './App.js';
+import App from './App';
 
 createRoot(document.getElementById('root')).render(<h1><App/></h1>)
